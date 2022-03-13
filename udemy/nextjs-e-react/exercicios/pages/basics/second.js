f849fc5a1de3b0a856408a49394c8a01360a7e15
Lucas Rocha <lucascosmorocha@gmail.com>
2022-03-13 11:10:08 -0300

    feat(nextjs-e-react): JSX #01

---
+++ udemy/nextjs-e-react/exercicios/pages/basics/second.js
@@ -0,0 +1,10 @@
+export default function second() {
+    return (
+        <div>
+            <h1>Hello World 1</h1>
+            <h2>Hello World 2</h2>
+            <h3>Hello World 3</h3>
+            <h4>Hello World 4</h4>
+        </div>
+    )
+}
