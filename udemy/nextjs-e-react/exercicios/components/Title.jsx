636c76c292bbb8f8045c75cb173f5362ca546516
Lucas Rocha <lucascosmorocha@gmail.com>
2022-03-13 12:36:50 -0300

    feat(nextjs-e-react): Propriedade Componente

--- udemy/nextjs-e-react/exercicios/components/Title.jsx
+++ udemy/nextjs-e-react/exercicios/components/Title.jsx
@@ -1,8 +1,14 @@
-export default function Title() {
+export default function Title(props) {
     return (
-        <>
-            <h1>Title</h1>
-            <h2>Subtitle</h2>
-        </>
+        props.small ?
+            <>
+                <h5>{props.main}</h5>
+                <h6>{props.secondary}</h6>
+            </> :
+
+            <>
+                <h1>{props.main}</h1>
+                <h2>{props.secondary}</h2>
+            </>
     )
 }
--- udemy/nextjs-e-react/exercicios/pages/basics/with-title.jsx
+++ udemy/nextjs-e-react/exercicios/pages/basics/with-title.jsx
@@ -3,7 +3,21 @@ import Title from "../../components/Title";
 export default function withTitle() {
     return (
         <div>
-            <Title/>
+            <Title
+                main={"Sign In Page"}
+                secondary={"Add, update and delete things!"}
+            />
+
+            <Title
+                main={"Login Page"}
+                secondary={"Please inform your username and password"}
+            />
+
+            <Title
+                main={"Login Page"}
+                secondary={"Please inform your username and password"}
+                small={true}
+            />
         </div>
     )
 }
