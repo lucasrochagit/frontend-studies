4a55cf99d6146febc629850aab708a4aec9d3c29
Lucas Rocha <lucascosmorocha@gmail.com>
2022-03-13 12:13:44 -0300

    feat(nextjs-e-react): Fragment #01

---
+++ udemy/nextjs-e-react/exercicios/pages/basics/fragment.jsx
@@ -0,0 +1,10 @@
+import {Fragment} from "react";
+
+export default function fragment() {
+    return (
+        <Fragment>
+            <h1>Title</h1>
+            <h2>Subtitle</h2>
+        </Fragment>
+    )
+}
