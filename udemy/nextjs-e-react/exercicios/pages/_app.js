424c5dd084ccf37b4a643abfe6a02b3e983f1f05
Lucas Rocha <lucascosmorocha@gmail.com>
2022-03-13 16:43:44 -0300

    feat(nextjs-e-react): Integração com CSS #01

--- udemy/nextjs-e-react/exercicios/pages/_app.js
+++ udemy/nextjs-e-react/exercicios/pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import './css/integration1.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return <Component {...pageProps} />
 }
 
 export default MyApp
---
+++ udemy/nextjs-e-react/exercicios/pages/css/integration1.jsx
@@ -0,0 +1,11 @@
+// Cannot import css here
+// import has done inside _app.js
+export default function integration() {
+    return (
+        <div className={"integration1"}>
+            <div className={"red"}>Text #01</div><br/>
+            <div className={"blue"}>Text #02</div><br/>
+            <div className={"white"}>Text #03</div><br/>
+        </div>
+    )
+}
