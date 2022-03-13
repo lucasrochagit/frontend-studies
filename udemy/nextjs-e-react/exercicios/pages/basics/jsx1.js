c73c892871a5c996677cd63ab8e9a87933d60c11
Lucas Rocha <lucascosmorocha@gmail.com>
2022-03-13 11:33:12 -0300

    feat(nextjs-e-react): JSX #02

---
+++ udemy/nextjs-e-react/exercicios/pages/basics/jsx1.js
@@ -0,0 +1,10 @@
+export default function jsx1() {
+    return (
+        <div>
+            <h1>JSX #01 1</h1>
+            <h2>JSX #01 2</h2>
+            <h3>JSX #01 3</h3>
+            <h4>JSX #01 4</h4>
+        </div>
+    )
+}
---
+++ udemy/nextjs-e-react/exercicios/pages/basics/jsx2.js
@@ -0,0 +1,11 @@
+export default function jsx2() {
+    const content = (
+        <div>
+            <h1>JSX #02 1</h1>
+            <h2>JSX #02 2</h2>
+            <h3>JSX #02 3</h3>
+            <h4>JSX #02 4</h4>
+        </div>
+    )
+    return content
+}
