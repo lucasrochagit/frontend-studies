6445269afb90aad7a2311a6c23c5c1d761ab9046
Lucas Rocha <lucascosmorocha@gmail.com>
2022-03-13 11:42:16 -0300

    feat(nextjs-e-react): JSX #03

---
+++ udemy/nextjs-e-react/exercicios/pages/basics/jsx4.jsx
@@ -0,0 +1,9 @@
+export default function jsx4() {
+    const subtitle = "I'm on Javascript!"
+    return (
+        <div>
+            <h1>Integração JS e JSX</h1>
+            <h2>{subtitle}</h2>
+        </div>
+    )
+}
